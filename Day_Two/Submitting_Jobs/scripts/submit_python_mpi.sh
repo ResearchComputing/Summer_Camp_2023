9de7a6a7813d9eb318ef5ff99f0eb83f15dd2474
Trevor Alan Hall <trha5176@colorado.edu>
2023-05-23 12:10:34 -0600

    mpi tweak

--- Day_Two/Submitting_Jobs/scripts/submit_python_mpi.sh
+++ Day_Two/Submitting_Jobs/scripts/submit_python_mpi.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-#SBATCH --nodes=1			# Number of requested nodes
-#SBATCH --ntasks=4			# Number of requested cores
-#SBATCH --time=0:01:00			# Max walltime
-#SBATCH --qos=normal	      		# Specify QOS
-#SBATCH --partition=shas		# Specify Summit haswell nodes
-#SBATCH --output=./output/python_%j.out		# Output file name
+#SBATCH --nodes=1                       # Number of requested nodes
+#SBATCH --ntasks=4                      # Number of requested cores
+#SBATCH --time=0:01:00                  # Max walltime
+#SBATCH --qos=normal
+#SBATCH --partition=amilan
+#SBATCH --constraint=ib
+#SBATCH --output=./output/python_%j.out         # Output file name
 
-# Written by:	Andrew Monaghan, 08 March 2018
-# Updated by:	Shelley Knuth, 17 May 2019
-# Purpose: 	To demonstrate how to submit an MPI python job 
+# Written by:   Andrew Monaghan, 08 March 2018
+# Updated by:   Trevor Hall, 21 May 2023
+# Purpose:      To demonstrate how to submit an MPI python job
 
 # purge all existing modules
 module purge
 
 # Load the python module
-module load python/3.5.1
-module load intel impi   
+module load python
+module load intel impi
+module load anaconda
+conda activate mycustomenv
 
 # Run Python Script
-cd programs 
+cd ./programs
 mpirun -np 4 python hello1.py
